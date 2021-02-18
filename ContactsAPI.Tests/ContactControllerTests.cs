f6dc66b6324062b31235b0d4ad6392e92936b034
Umut Dere <42785142+umutdr@users.noreply.github.com>
2021-02-18 20:37:08 +0300

    Integration test yapısı

---
+++ ContactsAPI.Tests/ContactControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using ContactsAPI.Contracts.V1;
+using ContactsAPI.Contracts.V1.Requests.Contact;
+using ContactsAPI.Domain;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContactsAPI.Tests
+{
+    public class ContactControllerTests : IntegrationTest
+    {
+        [Fact]
+        public async Task Get_WhileContactIdMatchesAnyRecord_ShouldReturnTheMatchingRecord()
+        {
+            // Arrange
+            await AuthenticatiAsync();
+            var createdTestContact
+                = await CreateContactAsync(
+                    new CreateContactRequest
+                    {
+                        FirstName = "Integration",
+                        LastName = "Test",
+                        CompanyName = "IntegrationTest"
+                    });
+
+            // Act
+            var response = await httpClient.GetAsync(APIRoutes.ContactControllerRoutes.Get.Replace("{contactId}", createdTestContact.Id.ToString()));
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedTestContact = await response.Content.ReadAsAsync<Contact>();
+
+            returnedTestContact.Id.Should().Be(createdTestContact.Id);
+            returnedTestContact.FirstName.Should().Be(createdTestContact.FirstName);
+            returnedTestContact.LastName.Should().Be(createdTestContact.LastName);
+            returnedTestContact.CompanyName.Should().Be(createdTestContact.CompanyName);
+        }
+
+        [Fact]
+        public async Task GetAll_WhileContactsTableInDbIsEmpty_ShouldReturnEmpty()
+        {
+            // Arrange
+            await AuthenticatiAsync();
+
+            // Act
+            var response = await httpClient.GetAsync(APIRoutes.ContactControllerRoutes.GetAll);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Contact>>()).Should().BeEmpty();
+        }
+    }
+}
---
+++ ContactsAPI.Tests/IntegrationTest.cs
@@ -0,0 +1,72 @@
+﻿using ContactsAPI.Contracts.V1;
+using ContactsAPI.Contracts.V1.Requests.Contact;
+using ContactsAPI.Contracts.V1.Requests.Identity;
+using ContactsAPI.Contracts.V1.Responses.Contact;
+using ContactsAPI.Contracts.V1.Responses.Identity;
+using ContactsAPI.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsAPI.Tests
+{
+    public class IntegrationTest
+    {
+        protected readonly HttpClient httpClient;
+
+        protected IntegrationTest()
+        {
+            var appFactory
+                = new WebApplicationFactory<Startup>()
+                    .WithWebHostBuilder(builder =>
+                    {
+                        // Bu blogu kaldırırsak test asıl veritabanını kullanacaktır.
+                        builder.ConfigureServices(services =>
+                        {
+                            // Projenin, asıl veritabanıyla iletişim kurmak için kullandığı servis devre dışı bırakıldı.
+                            services.RemoveAll(typeof(DataContext));
+
+                            services.AddDbContext<DataContext>(options =>
+                            {
+                                // Test ortamında asıl veritabanını kullanmak yerine, hafızada oluşturulan asıl veritabanının bir kopyası kullanılacak
+                                // Kopya veritabaninin ismini belirtiyor
+                                options.UseInMemoryDatabase("TestDatabase");
+                            });
+                        });
+                    });
+            httpClient = appFactory.CreateClient();
+        }
+
+        protected async Task AuthenticatiAsync()
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJWTAsync());
+        }
+
+        protected async Task<ContactResponse> CreateContactAsync(CreateContactRequest request)
+        {
+            var response = await httpClient.PostAsJsonAsync(APIRoutes.ContactControllerRoutes.Create, request);
+
+            return await response.Content.ReadAsAsync<ContactResponse>();
+        }
+
+        private async Task<string> GetJWTAsync()
+        {
+            var response = await httpClient.PostAsJsonAsync(APIRoutes.IdentityControllerRoutes.Register, new UserRegistrationRequest
+            {
+                Email = "integration@test.com",
+                Password = "Password123#"
+            });
+
+            var registrationResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+
+            return registrationResponse.Token; // test ortaminda kullanilacak JWT
+        }
+    }
+}
